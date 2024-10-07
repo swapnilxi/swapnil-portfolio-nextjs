c8b7d949af0256fbd02b73baeaa34c8ce5913b83
swapnil gupta <swapnilgupta.xi@gmail.com>
2024-10-07 18:49:37 +0530

    added card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { ThreeDCard } from '@/components/ThreeDCard'
 
 function HomePage() {
   return (
-    <div>HomePage</div>
+    <div>HomePage
+
+      {/* skills */}
+      <div className='flex justify-center items-center gap-10'>
+        <ThreeDCard title="Apple Vision Pro"/>
+        <ThreeDCard title="AI Development"/>
+        <ThreeDCard title="Full Stack Development"/>
+        <ThreeDCard title="DevOps and Cloud Engineering"/>
+       
+      </div>
+    </div>
   )
 }
 
